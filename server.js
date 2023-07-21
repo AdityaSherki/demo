bbc8b517091d452e9e57af7cf590a8875c09c3d7
Aditya <sherkiaditya@gmail.com>
2023-07-21 22:29:36 +0530

    First Push

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -6,15 +7,19 @@ const PORT = process.env.PORT || 8080;
 const baseUrl = '/calculator'
 
 app.use(express.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    const code = 200;
+    return res.send('Hello World! ' + code);
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const num1 = Number(req.body.first);
+    const num2 = Number(req.body.second);
+    const result = num1 + num2;
+    res.json({ "result": result });
 });
 
 
